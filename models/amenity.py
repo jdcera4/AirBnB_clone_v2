bf419d7ca0b1894c078b21dc191be19323f13a04
jdcera4 <jdcera4@gmail.com>
2021-08-10 20:36:07 -0400

    create table amenity and columns

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy.sql.schema import Column
+from sqlalchemy.sql.sqltypes import String
+from sqlalchemy.orm import relationship, backref
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    from models.place import place_amenity
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("PLace", secondary=place_amenity)
