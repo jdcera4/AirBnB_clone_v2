13407eab2c2dca67eddbaeeb3d98dc72a2cdbf2f
jdcera4 <jdcera4@gmail.com>
2021-08-11 17:41:54 -0400

    delete tests pep8

--- tests/test_console.py
+++ tests/test_console.py
@@ -13,16 +13,6 @@ from unittest.mock import create_autospec
 class test_console(unittest.TestCase):
     """ Test the console module """
 
-    def setUp(self):
-        '''setup for'''
-        self.backup = sys.stdout
-        self.capt_out = StringIO()
-        sys.stdout = self.capt_out
-
-    def tearDown(self):
-        ''''''
-        sys.stdout = self.backup
-
     def create(self):
         """ create instance of HBNBCommand class """
         return HBNBCommand()
