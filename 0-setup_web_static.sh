e36bafa5c312b7fcc8f2c393c06e6a4bf0d83fa1
jdcera4 <jdcera4@gmail.com>
2021-08-26 09:56:38 -0400

    task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,10 +1,32 @@
 #!/usr/bin/env bash
-# sets up your web servers for the deployment of web_static. It must
-sudo apt-get -y update
+# sets up your web servers for the deployment of web_static
+
+sudo apt-get update
 sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
-echo "This a test nginx" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current/
-sudo chown -hR ubuntu/ubuntu /data/
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx start
+sudo ufw allow 'Nginx HTTP'
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo chown -R "$USER":"$USER" /data/
+rm -rf /data/web_static/releases/test/index.html
+echo "
+<html>
+  <head>
+  </head>
+  <body>
+    <h1>Hello</h1>
+  </body>
+</html>
+" >> /data/web_static/releases/test/index.html
+rm -rf /data/web_static/current
+sudo ln -s /data/web_static/releases/test /data/web_static/current
+rm -rf /etc/nginx/sites-available/default
+echo "
+server {
+	listen 80 default_server;
+    listen [::]:80 default_server;
+    location /hbnb_static/ {
+        alias /data/web_static/current/;
+    }
+}
+" >> /etc/nginx/sites-available/default
+sudo service nginx restart
