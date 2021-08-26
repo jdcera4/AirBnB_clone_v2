55c9d4872ad40fea9be307adbd7448d745745ef4
jdcera4 <jdcera4@gmail.com>
2021-08-26 10:22:52 -0400

    add -p

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,7 +9,7 @@ def do_pack():
     now = datetime.datetime.now()
     file = ('versions/web_static_{}{}{}{}{}{}.tgz'.format(
         now.year, now.month, now.day, now.hour, now.minute, now.second))
-    local("mkdir versions")
+    local("mkdir -p versions")
     path = "web_static"
     review = local("tar -czvf {} {}".format(file, path))
 
