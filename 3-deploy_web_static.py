20ded9fb307bfa4cb72b8545f2bda46b14417e65
jdcera4 <jdcera4@gmail.com>
2021-08-26 20:11:33 -0500

    Deploy to servers

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+""" Fabric File """
+from fabric.api import *
+from datetime import datetime
+import os.path
+env.hosts = ['34.74.126.117', '34.235.137.233']
+
+
+def deploy():
+    """ Deploy to servers """
+    try:
+        archive_path = do_pack()
+    except:
+        return False
+    return do_deploy(archive_path)
+
+
+def do_deploy(archive_path):
+    """ Deploy an archive """
+
+    if not os.path.exists(archive_path):
+        return False
+
+    try:
+        file_name = archive_path[9:]
+        Extension = file_name[:-4]
+
+        put(archive_path, '/tmp/' + file_name)
+        run("mkdir -p /data/web_static/releases/" + Extension)
+        run('tar -xzvf /tmp/' + file_name +
+            " -C /data/web_static/releases/" +
+            Extension + " --strip-components=1")
+        run("rm -f /tmp/" + file_name)
+        run("rm -f /data/web_static/current")
+        run("sudo ln -sf /data/web_static/releases/" +
+            Extension + " /data/web_static/current")
+
+        return True
+    except:
+        return False
+
+
+def do_pack():
+    """ function do pack """
+
+    try:
+        now = datetime.now()
+
+        file_name = "web_static_" + now.strftime("%Y%m%d%H%M%S") + ".tgz"
+        name_path = "versions/" + file_name
+
+        local("mkdir -p versions")
+
+        local("tar -czvf " + name_path + " web_static")
+
+        return name_path
+    except:
+        return None
